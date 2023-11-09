26c7122d59b5bbb2b1606da9036a05a786b28333
Aleksei <125027733+Lek14@users.noreply.github.com>
2023-11-09 16:29:48 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+const skillsMember = () => {
+    const skills = document.querySelectorAll('.skill');
+    skills.forEach((skill) => {
+        skill.addEventListener('click', (event) => {
+        event.currentTarget.classList.toggle('active');
+        });
+    });     
+}
